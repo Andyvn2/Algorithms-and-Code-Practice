7b73fd4ce4be44735b01881e92778a0f4927178a
Andy <andyvn2@uci.edu>
2018-09-24 08:51:07 -0700

    firstNotRepeatingCharacter

--- firstNotRepeatingCharacter.py
+++ firstNotRepeatingCharacter.py
@@ -1,4 +1,5 @@
-
+# Note: Write a solution that only iterates over the string once and uses O(1) 
+# additional memory, since this is what you would be asked to do during a real interview.
 
 def firstNotRepeatingCharacter(s):
     for a in range(len(s)):
